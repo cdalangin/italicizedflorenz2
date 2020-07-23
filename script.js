65d59682eaada1d175080c4cfe4ac2870845b7f7
cdalangin <florenzdalangin@gmail.com>
2020-07-23 10:20:49 -0400

    made second section

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-var hotbod = document.querySelector("body");
-
-function doStuff() {
-    hotbod.className += " animate";
+// folder changes
+function mySchool() {
+    var x = document.getElementById("info1");
+    if (x.style.visibility === "visible") {
+        x.style.visibility = "hidden";
+    } else {
+        x.style.visibility = "visible";
+    }
 }
 
-window.onload = function () {
-    doStuff();
-};
+function myActivities() {
+    var x = document.getElementById("info2");
+    if (x.style.visibility === "visible") {
+        x.style.visibility = "hidden";
+    } else {
+        x.style.visibility = "visible";
+    }
+}
 
+function myOther() {
+    var x = document.getElementById("info3");
+    if (x.style.visibility === "visible") {
+        x.style.visibility = "hidden";
+    } else {
+        x.style.visibility = "visible";
+    }
+}
